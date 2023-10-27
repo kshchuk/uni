f1d924da24531b4472d0a8c99af9c0756d62257f
kshchuk <kshchuk@gmail.com>
2023-10-28 00:23:11 +0300

    Create functions.go

--- 5-sem/operating-systems/lab-1/function-1/model/function.go
+++ 5-sem/operating-systems/lab-1/function-1/model/function.go
@@ -1,22 +1,3 @@
 package model
 
-import (
-	"fmt"
-	"time"
-)
-
 type Function func(errChan chan error, args ...interface{}) (interface{}, error)
-
-/*
-Function1 is the function that will be executed to compute some value.
-
-errChan is a channel that can be used to send non-critical errors back to the controller.
-*/
-func Function1(errChan chan error, args ...interface{}) (interface{}, error) {
-	time.Sleep(5 * time.Second) // simulate long running task
-
-	errChan <- fmt.Errorf("non-critical error occurred")
-
-	time.Sleep(5 * time.Second) // simulate long running task
-	return args[0], nil
-}
