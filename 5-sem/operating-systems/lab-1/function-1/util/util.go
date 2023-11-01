1720ce2850f48724822957a1cda26c0d8462941e
kshchuk <kshchuk@gmail.com>
2023-11-01 15:20:33 +0200

    Updated int serialization & added tests

--- 5-sem/operating-systems/lab-1/function-1/util/util.go
+++ 5-sem/operating-systems/lab-1/function-1/util/util.go
@@ -1,12 +1,33 @@
 package util
 
-import "unsafe"
+import (
+	"bytes"
+	"encoding/gob"
+	"reflect"
+)
 
-func GetSize[T any]() uintptr {
-	var v T
-	return unsafe.Sizeof(v)
+func GetSize[T any](v T) uintptr {
+	rv := reflect.ValueOf(v)
+	return rv.Type().Size()
 }
 
-func ToBytes[T any](v T) []byte {
-	return *(*[]byte)(unsafe.Pointer(&v))
+func ToBytes(v int64) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func FromBytes(data []byte) (int64, error) {
+	var v int64
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(&v)
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
 }
