34a5fe1d5b93ee9c6f48ecdb92c8f185f94a03a3
kshchuk <kshchuk@gmail.com>
2023-11-20 23:22:32 +0200

    implemented handler

--- 5-sem/OOP/labs/lab-2/src/main/java/com/example/Strings.java
+++ 5-sem/OOP/labs/lab-2/src/main/java/com/example/Strings.java
@@ -5,7 +5,7 @@ public class Strings {
     public static final String DEVICE = "device";
     public static final String ID = "id";
     public static final String NAME = "name";
-    public static final String origin = "origin";
+    public static final String ORIGIN = "origin";
     public static final String PRICE = "price";
     public static final String CRITICAL = "critical";
     public static final String TYPE = "type";
