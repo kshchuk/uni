6984dec5d40c397a4e118bc0f7d3ed00039cd04e
kshchuk <kshchuk@gmail.com>
2023-11-28 18:38:03 +0200

    borrowed some code to test architecture

--- 5-sem/ruby/lab-7/RoR_Project/db/seeds.rb
+++ 5-sem/ruby/lab-7/RoR_Project/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Student.create(
+  name: 'Liz Valley',
+  age: 20,
+  dorm: true,
+  language: 'Python',
+  teacher: false,
+  university: 'MNK University'
+)
