ac2636b034cc86eb35ca648a130660bc580f62a0
Yaroslav Kishchuk <98614059+kshchuk@users.noreply.github.com>
2024-05-01 19:44:23 +0300

    init front

--- 6-sem/OOP/Lab-1/public-utilities-servlets-api/src/main/java/org/example/entity/Specialist.java
+++ 6-sem/OOP/Lab-1/public-utilities-servlets-api/src/main/java/org/example/entity/Specialist.java
@@ -6,13 +6,15 @@ import lombok.Data;
 import java.util.List;
 import java.util.UUID;
 
+/// Class that represents a specialist in the system (dispatcher or worker)
+
 @Data
 @AllArgsConstructor
 public class Specialist {
     private UUID specialistId;
     private String name;
     private String specialization;
-    private Team team;
+    private Team team; // if the specialist is not a dispatcher
 
     private List<WorkPlan> workPlans;
 
