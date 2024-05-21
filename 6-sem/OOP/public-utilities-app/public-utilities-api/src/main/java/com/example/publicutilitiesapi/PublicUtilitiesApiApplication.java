1a7afae2c17a36cddd6f4d2d58c1c91407cfcf10
Yaroslav Kishchuk <98614059+kshchuk@users.noreply.github.com>
2024-05-21 15:04:29 +0300

    init proj

---
+++ 6-sem/OOP/public-utilities-app/public-utilities-api/src/main/java/com/example/publicutilitiesapi/PublicUtilitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicutilitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicUtilitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicUtilitiesApiApplication.class, args);
+	}
+
+}
---
+++ 6-sem/OOP/public-utilities-app/public-utilities-api/src/test/java/com/example/publicutilitiesapi/PublicUtilitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicutilitiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicUtilitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
